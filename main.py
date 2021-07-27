cca8bf197df38d3a3db4ccd7181e9fa339ba57e1
Bao Loc Pham <phambaoloc163@gmail.com>
2021-07-27 15:42:50 +0700

    Fix error: could not load img

--- main.py
+++ main.py
@@ -96,34 +96,35 @@ def count_down(count):
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+if __name__ == '__main__':
+    window = Tk()
+    window.title("Pomodoro")
+    window.config(padx=100, pady=50, bg=YELLOW)
 
-# Timer text label
-header = Label(text="Timer", font=(FONT_NAME, 24, "bold"), fg=GREEN, highlightthickness=0, bg=YELLOW)
-header.grid(row=0, column=1)
+    # Timer text label
+    header = Label(text="Timer", font=(FONT_NAME, 24, "bold"), fg=GREEN, highlightthickness=0, bg=YELLOW)
+    header.grid(row=0, column=1)
 
-# tomato
-canvas = Canvas(width=208, height=224, bg=YELLOW, highlightthickness=0)
-# read img in tk
-path = resource_path("C:/StudyingandExaminations/Python/100Days project/Day28/Pomodoro/img/tomato.png")
-print(path)
-img_tomato = PhotoImage(file=path)
-canvas.create_image(101, 112, image=img_tomato)
-timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
-canvas.grid(row=1, column=1)
+    # tomato
+    canvas = Canvas(width=208, height=224, bg=YELLOW, highlightthickness=0)
+    # read img in tk
+    path = resource_path("./img/tomato.png")
+    print(path)
+    img_tomato = PhotoImage(file=path)
+    canvas.create_image(101, 112, image=img_tomato)
+    timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
+    canvas.grid(row=1, column=1)
 
-# start button
-start_button = Button(text="Start", font=(FONT_NAME, 10, "normal"), command=start_timer)
-start_button.grid(row=2, column=0)
-# reset button
-reset_button = Button(text="Reset", font=(FONT_NAME, 10, "normal"), command=reset_timer)
-reset_button.grid(row=2, column=2)
+    # start button
+    start_button = Button(text="Start", font=(FONT_NAME, 10, "normal"), command=start_timer)
+    start_button.grid(row=2, column=0)
+    # reset button
+    reset_button = Button(text="Reset", font=(FONT_NAME, 10, "normal"), command=reset_timer)
+    reset_button.grid(row=2, column=2)
 
-# Check mark text Label
-check_mark = Label(text="", fg=GREEN, font=(FONT_NAME, 10, "bold"), highlightthickness=0, bg=YELLOW)
-check_mark.grid(row=3, column=1)
+    # Check mark text Label
+    check_mark = Label(text="", fg=GREEN, font=(FONT_NAME, 10, "bold"), highlightthickness=0, bg=YELLOW)
+    check_mark.grid(row=3, column=1)
 
-#
-window.mainloop()
+    #
+    window.mainloop()
